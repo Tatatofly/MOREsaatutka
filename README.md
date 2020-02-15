155429329c244e090c3a0d5863bc5913b7283a37
Tatu Toikkanen <tatatofly@gmail.com>
2020-02-15 12:52:42 +0200

    React App Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# MORE Säätutka
+Etteplan MOREn Front end -ennakkotehtävä 2020 Kevät - MOREn säätutka
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">Säätutka</div>
+      <div className="content"></div>
+    </div>
+  );
+}
+
+export default App;
